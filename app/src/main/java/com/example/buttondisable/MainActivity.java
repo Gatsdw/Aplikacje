df2fbf988d424c8a5d63586686b10a3729182d07
bglow <b.glowacki123456@gmail.com>
2024-10-02 18:13:38 +0200

    Projekt Zad 1

---
+++ app/src/main/java/com/example/buttondisable/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.buttondisable;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonDisable;
+    private Button buttonEnable;
+    int counter = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buttonDisable = findViewById(R.id.buttonDisable);
+
+        buttonDisable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                buttonDisable.setEnabled(false);
+                Toast.makeText(MainActivity.this, "Przycisk został wyłączony", Toast.LENGTH_SHORT).show();
+                counter++;
+            }
+        });
+        buttonEnable = findViewById(R.id.buttonEnable);
+
+        buttonEnable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                buttonDisable.setEnabled(true);
+                Toast.makeText(MainActivity.this, "Przycisk został włączony "+counter+" razy.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
